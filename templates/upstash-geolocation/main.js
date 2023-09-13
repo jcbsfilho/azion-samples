e6289a5f2b8ee6f261326033f91336e634cf1f5f
Jose Filho <jc.bsfilho@gmail.com>
2023-09-13 18:15:03 -0300

    fix: change handler to new version vulcan

--- templates/upstash-geolocation/main.js
+++ templates/upstash-geolocation/main.js
@@ -1,11 +1,6 @@
 import { handleRequest } from "./src/function/index.js";
 
-return await handleRequest(event?.request, event?.args);
+async function main(event){
+    return handleRequest(event?.request, event?.args);
+}
 
-// async function handleEvent(event) {
-//   __HANDLER__;
-// }
-
-// addEventListener("fetch", (event) => {
-//   event.respondWith(handleEvent(event));
-// });
